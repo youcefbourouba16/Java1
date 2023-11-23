ebcdda51ed5cfd7ead52845e00762034af338db2
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-11-23 11:17:13 +0100

    .

---
+++ s2/project2_calculatrice/src/main/java/com/mycompany/project2_calculatrice/Project2_calculatrice.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project2_calculatrice;
+
+/**
+ *
+ * @author youcef
+ */
+public class Project2_calculatrice {
+
+    public static void main(String[] args) {
+        calculatrice g=new calculatrice();
+         g.pack();
+         g.setLocationRelativeTo(null);
+         g.setVisible(true);
+    }
+}
