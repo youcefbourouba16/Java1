87ea3ba0a298655658c1310ff8e04b6cffa3f49a
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-12-12 01:12:33 +0100

    .

---
+++ s2/Resirvation/src/main/java/Model/Model1.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author youcef
+ */
+public class Model1 {
+
+    public Model1(String Name, String Prenom, String Depui, String Distination, Date Selected_day, String Class, String AR, String Documment, int Num_Ident, Date Date_Declaration, Date Date_Experation, int Bagage) {
+        this.Name = Name;
+        this.Prenom = Prenom;
+        this.Depui = Depui;
+        this.Distination = Distination;
+        this.Selected_day = Selected_day;
+        this.Class = Class;
+        this.AR = AR;
+        this.Documment = Documment;
+        this.Num_Ident = Num_Ident;
+        this.Date_Declaration = Date_Declaration;
+        this.Date_Experation = Date_Experation;
+        this.Bagage = Bagage;
+    }
+    public String Name;
+    public String Prenom;
+    public String Depui;
+    public String Distination;
+    public Date Selected_day;
+    public String Class;
+    public String AR;
+    public String Documment;
+    public int Num_Ident;
+    public Date Date_Declaration;
+    public Date Date_Experation;
+    
+    public int Bagage;
+    
+    
+    
+    
+    
+    
+}
---
+++ s2/Resirvation/src/main/java/com/mycompany/resirvation/Resirvation.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.resirvation;
+
+/**
+ *
+ * @author youcef
+ */
+public class Resirvation {
+
+    public static void main(String[] args) {
+       ResirvationUI g=new ResirvationUI();
+         g.pack();
+         g.setLocationRelativeTo(null);
+         g.setVisible(true);
+        
+    }
+}
